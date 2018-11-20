fc13f3499fcf662f9cfca19ce565c1e0563fdbb9
LuWei <318679507@qq.com>
2018-11-20 23:28:25 +0800

    绘制menu面板

--- app/src/main/java/com/loosu/circlemenu/CircleMenu.java
+++ app/src/main/java/com/loosu/circlemenu/CircleMenu.java
@@ -7,19 +7,23 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class CircleMenu extends ViewGroup {
+    private static final String TAG = "CircleMenu";
 
     private float mRadiusMax = 500;
-    private float mRadiusMin = 200;
+    private float mRadiusMin = 0;
 
     private float mRadius = mRadiusMin;
 
     private boolean isOpen = false;
 
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private View mActionView = null;
 
     public CircleMenu(Context context) {
         this(context, null);
@@ -31,28 +35,35 @@ public class CircleMenu extends ViewGroup {
 
     public CircleMenu(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setWillNotDraw(false);
+        this.setWillNotDraw(false);
         this.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+
+        ImageView actionView = new ImageView(context);
+        actionView.setImageResource(R.mipmap.ic_launcher_round);
+        mActionView = actionView;
+        addView(actionView);
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        Log.i(TAG, "onLayout: changed = " + changed + ", left = " + l + ", top = " + t + ", right = " + r + ", bottom = " + b);
+        int centerX = (r - l) / 2;
+        int centerY = (b - t) / 2;
+        mActionView.layout(centerX - 50, centerY - 50,centerX + 50, centerY + 50 );
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        float left = getWidth() - mRadius;
-        float top = getHeight() - mRadius;
-        float right = getWidth() + mRadius;
-        float bottom = getHeight() + mRadius;
+        float left = getWidth() / 2 - mRadius;
+        float top = getHeight() / 2 - mRadius;
+        float right = getWidth() / 2 + mRadius;
+        float bottom = getHeight() / 2 + mRadius;
         RectF panelRect = new RectF(left, top, right, bottom);
 
         mPaint.setShadowLayer(10f, -10, -10, Color.RED);
         canvas.drawArc(panelRect, 0, 360, true, mPaint);
     }
-
 
     public void open(boolean animated) {
         isOpen = true;
@@ -72,7 +83,7 @@ public class CircleMenu extends ViewGroup {
 
     public void setRadius(float radius) {
         mRadius = radius;
-        postInvalidate();
+        invalidate();
     }
 
     public boolean isOpen() {
